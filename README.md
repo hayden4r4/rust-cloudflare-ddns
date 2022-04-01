863f9fe5ec26420e2632dea16dbca4c3140e6df1
Hayden Rose <hayden4r4@gmail.com>
2022-03-31 19:30:39 -0700

    updated README and added comments to code

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This is a simple script that updates your DNS domain's IP when the host IP changes.  The only setup required is to create a new entry 'A' (for ipv4) or 'AAAA' (for ipv6) entry in cloudlfare with your desired name (ex. name=ddns for ddns.mywebsite.com) and 0.0.0.0 for content with proxy disabled.  Then create a .env file with your settings in the root of the project folder. Finally, schedule this script to run at desired interval (I recommend between 5-30 mins).  
   
 ### The .env file should be set up as follows: ###  
-TOKEN: a string of your cloudlfare api key configured with the 'edit zone dns' template and with your domain name selected as the zone resource.  This string should be appended by 'Bearer '.  This is used to authenticate with the cloudflare API. ex: 'Bearer dslfhasd34fsfsdlfhlska'  
-TYPE: a string, either 'A' or 'AAAA' for ipv4 or ipv6 respectively.  
-RECORD: a string of the full url of your domain.  ex: 'ddns.mywebsite.com'  
-ZONEID: a string of your Zone ID which can be found on the overview page of your domain on cloudflare's website.  
-ID: a string of your identifier.  This is the most difficult to get however it is static so only needs to be obtained once.  I recommend making a quick curl call with the method [here](https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records).  Here is the call for reference, fill in the <> with your info, look for the 'id' field for your RECORD:  
+**TOKEN**: a string of your cloudlfare api key configured with the 'edit zone dns' template and with your domain name selected as the zone resource.  This string should be appended by 'Bearer '.  This is used to authenticate with the cloudflare API. ex: TOKEN='Bearer dslfhasd34fsfsdlfhlska'  
+**TYPE**: a string, either 'A' or 'AAAA' for ipv4 or ipv6 respectively.  ex. TYPE='A'
+**RECORD**: a string of the full url of your domain.  ex: RECORD='ddns.mywebsite.com'  
+**ZONEID**: a string of your Zone ID which can be found on the overview page of your domain on cloudflare's website.  
+**ID**: a string of your identifier.  This is the most difficult to get however it is static so only needs to be obtained once.  I recommend making a quick curl call with the method [here](https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records).  *Here is the call for reference, fill in the <> with your info, look for the 'id' field for your RECORD:*  
     curl -X GET "https://api.cloudflare.com/client/v4/zones/<ZONE_ID>/dns_records?type=<TYPE>&name=<RECORD>&content=0.0.0.0" \  
     -H "Authorization: <TOKEN>" \  
     -H "Content-Type: application/json"  
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,21 @@ use serde_json::json;
 use tokio;
 
 async fn get_ip() -> Result<String, i32> {
+    /// asynchronously polls services to find public IP,
+    /// returns first it gets back.  Uses public_ip crate
     let s: String = if let Some(ip) = public_ip::addr().await {
         format!("{:?}", ip)
     } else {
+        // Safely exits if there is an error obtaining IP
         std::process::exit(1);
     };
     Ok(s)
 }
 
 async fn get_domain_ip() -> Result<serde_json::Value, reqwest::Error> {
+    /// sends get request to cloudflare API to obtain the current
+    /// IP content on the domain.  Formates response with serde json
+    /// and returns the value.
     let client: reqwest::Client = reqwest::Client::new();
     let resp = client
         .get(format!(
@@ -26,9 +32,10 @@ async fn get_domain_ip() -> Result<serde_json::Value, reqwest::Error> {
         .header("content-Type", "application/json")
         .send()
         .await?;
-
+    // Creates json from the response
     let result: serde_json::Value = match serde_json::from_str(&resp.text().await?) {
         Ok(r) => r,
+        // Safely exits if there is an error creating json
         Err(_) => std::process::exit(1),
     };
 
@@ -38,6 +45,10 @@ async fn get_domain_ip() -> Result<serde_json::Value, reqwest::Error> {
 }
 
 async fn set_domain_ip(ip: &str) -> Result<reqwest::Response, reqwest::Error> {
+    /// sets the cloudflare domain IP through a put
+    /// request after it is determined that the
+    /// current public IP != the current domain IP
+    // Creates json for the payload that is sent in the put request
     let data = json!({
             "type": dotenv!("TYPE"),
             "name": dotenv!("RECORD"),
@@ -46,7 +57,6 @@ async fn set_domain_ip(ip: &str) -> Result<reqwest::Response, reqwest::Error> {
             "proxied": true});
 
     let client: reqwest::Client = reqwest::Client::new();
-
     let resp = client
         .put(format!(
             "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
@@ -64,21 +74,25 @@ async fn set_domain_ip(ip: &str) -> Result<reqwest::Response, reqwest::Error> {
 
 #[tokio::main]
 async fn main() {
+    // obtains current public IP
     let ip: String = match get_ip().await {
         Ok(ip) => ip,
         Err(_) => std::process::exit(1),
     };
-
+    // obtains current domain IP as a Result<>
     let dom_ip: serde_json::Value = match get_domain_ip().await {
         Ok(dom_ip) => dom_ip,
         Err(_) => std::process::exit(1),
     };
-
+    // converts the current domain IP to a String
     let domain_ip: String = match dom_ip.as_str() {
         Some(domain_ip) => String::from(domain_ip),
         None => std::process::exit(1),
     };
-
+    /// Checks if curretn public IP is different than current domain IP,
+    /// then sends put request to update the domain IP to the current
+    /// public IP if they are different and then exits safely, or will
+    /// exit safely if they are the same.  Exits safely upon error.
     if ip != domain_ip {
         match set_domain_ip(&ip).await {
             Ok(_) => std::process::exit(0),
